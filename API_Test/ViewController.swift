8a7be9bf24ddf3b003e72b8e927947b56fc02e81
Do Hyeong Seok <zxc1460@naver.com>
2020-10-25 16:54:16 +0900

    Initial Commit

---
+++ API_Test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  API_Test
+//
+//  Created by Seok on 2020/10/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
